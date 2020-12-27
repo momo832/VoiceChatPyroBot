5a2b3e021d5555ea10ec0c34beb273ccab4217ce
momo832 <67990067+momo832@users.noreply.github.com>
2020-12-27 14:32:34 -0500

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,7 +3,10 @@ from pyrogram.handlers import MessageHandler
 
 
 async def start(client, message):
-    await message.reply_text("Hi, send me a YouTube link to play it.")
+    await message.reply_text("היי, שלח לי קישור לשיר / סרטון מיוטויב ואני אשמיע אותו בקבוצה: 
+@radio_ilGroup
+
+הבוט רץ ע"י @Yishaicohen")
 
 __handlers__ = [
     [
